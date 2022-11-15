486c51bbbf8359cf42311327f4fc45cd72900d9a
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-11-15 22:00:28 +0200

    very first draft

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,14 +13,14 @@ import {
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Seo title="Joshua Pearson - Game Producer" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
-        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        {/*<ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />*/}
+        <AboutSection sectionId="about" heading="About Me" />
+        <InterestsSection sectionId="details" heading="Skills" />
+        <ProjectsSection sectionId="features" heading="Projects" />
+        <ContactSection sectionId="github" heading="Contact" />
       </Page>
     </>
   );
